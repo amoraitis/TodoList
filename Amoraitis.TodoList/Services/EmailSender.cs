889744e7c4541dcea94ff79eef154bd567226e2e
Anastasios Moraitis <anas.moraitis@gmail.com>
2018-10-19 00:51:13 +0300

    Implement EmailSender with SendGrid

--- Amoraitis.TodoList/Services/EmailSender.cs
+++ Amoraitis.TodoList/Services/EmailSender.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,9 +12,31 @@ namespace Amoraitis.TodoList.Services
     // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string message)
+        private readonly IConfiguration Configuration;
+
+        public EmailSender(IConfiguration configuration)
         {
-            return Task.CompletedTask;
+            Configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            if (string.IsNullOrEmpty(Configuration["SendGrid:ServiceApiKey"])) return;
+
+            var apiKey = Configuration["SendGrid:ServiceApiKey"];
+
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage();
+            msg.SetFrom(new EmailAddress("noreply@amoraitis.todolist.com", "Amoraitis.TodoList Team"));
+            msg.AddTo(new EmailAddress(email));
+            msg.AddContent(MimeType.Html, message);
+            msg.SetSubject(subject);
+
+            var result = await client.SendEmailAsync(msg);
+            
+            if (result.StatusCode == System.Net.HttpStatusCode.OK)
+                return;
+
         }
     }
 }
