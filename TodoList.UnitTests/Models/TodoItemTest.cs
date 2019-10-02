0e3fbed08f783e71438e6c5fcf27e5621a551b43
Remco Eerden <remco.e@hotmail.com>
2019-10-02 17:15:52 +0200

    Removed comments for code coverage improvement check

--- TodoList.UnitTests/Models/TodoItemTest.cs
+++ TodoList.UnitTests/Models/TodoItemTest.cs
@@ -10,66 +10,66 @@ namespace TodoList.UnitTests.Models
 {
     public class TodoItemTest
     {
-        //[Fact]
-        //public void TodoItem_AllPropertiesCorrect_Ok()
-        //{
-        //    var validationResults = new List<ValidationResult>();
-        //    var todoItemId = Guid.NewGuid();
-        //    var todoItem = new TodoItem()
-        //    {
-        //        Id = todoItemId,
-        //        Title = "Cleaning",
-        //        Content = "Clean the kitchen.",
-        //        Added = new Instant(),
-        //        DueTo = new Instant(),
-        //        AddedDateTime = DateTime.Now,
-        //        DuetoDateTime = DateTime.Now,
-        //        Done = false,
-        //        UserId = Guid.NewGuid().ToString(),
-        //        File = new FileInfo()
-        //        {
-        //            Path = "C:/temp",
-        //            Size = 100,
-        //            TodoId = todoItemId
-        //        }
-        //    };
+        [Fact]
+        public void TodoItem_AllPropertiesCorrect_Ok()
+        {
+            var validationResults = new List<ValidationResult>();
+            var todoItemId = Guid.NewGuid();
+            var todoItem = new TodoItem()
+            {
+                Id = todoItemId,
+                Title = "Cleaning",
+                Content = "Clean the kitchen.",
+                Added = new Instant(),
+                DueTo = new Instant(),
+                AddedDateTime = DateTime.Now,
+                DuetoDateTime = DateTime.Now,
+                Done = false,
+                UserId = Guid.NewGuid().ToString(),
+                File = new FileInfo()
+                {
+                    Path = "C:/temp",
+                    Size = 100,
+                    TodoId = todoItemId
+                }
+            };
             
-        //    var isValid = Validator.TryValidateObject(todoItem, new ValidationContext(todoItem), validationResults);
+            var isValid = Validator.TryValidateObject(todoItem, new ValidationContext(todoItem), validationResults);
 
-        //    Assert.True(isValid);
-        //    Assert.Empty(validationResults);
-        //}
+            Assert.True(isValid);
+            Assert.Empty(validationResults);
+        }
 
-        //[Fact]
-        //public void TodoItem_UserIdIsRequired()
-        //{
-        //    var validationResults = new List<ValidationResult>();
-        //    var todoItemId = Guid.NewGuid();
-        //    var todoItem = new TodoItem()
-        //    {
-        //        Id = todoItemId,
-        //        Title = "Cleaning",
-        //        Content = "Clean the kitchen.",
-        //        Added = new Instant(),
-        //        DueTo = new Instant(),
-        //        AddedDateTime = DateTime.Now,
-        //        DuetoDateTime = DateTime.Now,
-        //        Done = false,
-        //        UserId = null, // Required
-        //        File = new FileInfo()
-        //        {
-        //            Path = "C:/temp",
-        //            Size = 100,
-        //            TodoId = todoItemId
-        //        }
-        //    };
+        [Fact]
+        public void TodoItem_UserIdIsRequired()
+        {
+            var validationResults = new List<ValidationResult>();
+            var todoItemId = Guid.NewGuid();
+            var todoItem = new TodoItem()
+            {
+                Id = todoItemId,
+                Title = "Cleaning",
+                Content = "Clean the kitchen.",
+                Added = new Instant(),
+                DueTo = new Instant(),
+                AddedDateTime = DateTime.Now,
+                DuetoDateTime = DateTime.Now,
+                Done = false,
+                UserId = null, // Required
+                File = new FileInfo()
+                {
+                    Path = "C:/temp",
+                    Size = 100,
+                    TodoId = todoItemId
+                }
+            };
 
-        //    var isValid = Validator.TryValidateObject(todoItem, new ValidationContext(todoItem), validationResults);
+            var isValid = Validator.TryValidateObject(todoItem, new ValidationContext(todoItem), validationResults);
 
-        //    Assert.False(isValid);
-        //    Assert.NotEmpty(validationResults);
-        //    Assert.Single(validationResults);
-        //    Assert.Equal($"The {nameof(TodoItem.UserId)} field is required.", validationResults.Single().ErrorMessage);
-        //}
+            Assert.False(isValid);
+            Assert.NotEmpty(validationResults);
+            Assert.Single(validationResults);
+            Assert.Equal($"The {nameof(TodoItem.UserId)} field is required.", validationResults.Single().ErrorMessage);
+        }
     }
 }
