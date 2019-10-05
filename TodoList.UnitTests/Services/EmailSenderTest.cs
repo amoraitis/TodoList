fc34970f732fcdfb917dc97610e536b469d9fb77
Gokilarajan <gokilarajan@gmail.com>
2019-10-05 14:18:08 +0530

    This commit has some comments added to the unit test

--- TodoList.UnitTests/Services/EmailSenderTest.cs
+++ TodoList.UnitTests/Services/EmailSenderTest.cs
@@ -38,14 +38,15 @@ namespace TodoList.UnitTests.Services
         [Fact]
         public async Task Should_Not_Throw_Exception_If_Can_Not_Send_Email()
         {
+            //Arrange
             _sendGridClientMock
                .Setup(client => client.SendEmailAsync(It.IsAny<SendGridMessage>(), It.IsAny<CancellationToken>()))
                .ReturnsAsync(new Response(System.Net.HttpStatusCode.InternalServerError, _httpContentMock.Object, null));
 
             EmailSender _emailSenderService = new EmailSender(_sendGridClientMock.Object, new SendGridMessage());
+            //Act
             await _emailSenderService.SendEmailAsync("max@example.com", "This is a test", "A test body");
+            // This test doesn't make any assertion, basically here we're checking no exception is thrown
         }
-
-        // TODO: Implement Should_Not_Throw_Exception_If_Can_Not_Send_Email() test
     }
 }
