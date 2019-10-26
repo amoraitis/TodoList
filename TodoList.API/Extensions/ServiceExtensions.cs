89582d081336e24539c2178eea9e0b7aed5775ab
Jenia <genia90@gmail.com>
2019-10-26 16:05:10 +0300

    Initial API commit

---
+++ TodoList.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using TodoList.Core.Contexts;
+
+namespace TodoList.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        /// <summary>
+        /// Configures Entity Framework DbContext for the project.
+        /// </summary>
+        public static void ConfigureEntityFramework(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddEntityFrameworkSqlServer().AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+        }
+
+        public static void ConfigureLogging(this IServiceCollection services, IConfiguration configuration)
+        {
+            // TODO: Is it really needed
+        }
+
+        /// <summary>
+        /// Configures API spec by using swagger.
+        /// </summary>
+        /// <param name="services"></param>
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info {Title = "TodoList API", Version = "v1"});
+            });
+        }
+
+        /// <summary>
+        /// Configures JSON Web Token authentication scheme.
+        /// </summary>
+        public static void ConfigureJwtAuthentication(this IServiceCollection services)
+        {
+            services.AddAuthentication().AddJwtBearer(); // TODO: Probably needs more work.
+        }
+    }
+}
---
+++ TodoList.API/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TodoList.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args)
+                .ConfigureAppConfiguration((hc, config) =>
+                {
+                    var env = hc.HostingEnvironment;
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                })
+                .ConfigureLogging((hc, logging) =>
+                {
+                    logging.AddConfiguration(hc.Configuration.GetSection("logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
+                })
+                .Build()
+                .Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
