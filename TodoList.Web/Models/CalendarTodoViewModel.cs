ef22828d0298e82354e16fa4d4e556e6cb55d0d6
pteberf <fatal1978@protonmail.com>
2020-04-18 16:23:23 +0300

    [#64] - Add TODO View Model for calendar usage.

---
+++ TodoList.Web/Models/CalendarTodoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoList.Web.Models
+{
+    public class CalendarTodoViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int DueToDay { get; set; }
+    }
+}
